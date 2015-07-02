b859bf7d99aa277ef2cadee6bd2326aa08f6a116
Bill Mulholland <billmulholland@Bills-iMac-2.local>
2015-07-02 19:34:06 +0100

    local

--- wp-content/themes/Your-Legal-Route_R2/library/parts/carousel.php
+++ wp-content/themes/Your-Legal-Route_R2/library/parts/carousel.php
@@ -1,15 +1,19 @@
 <!--This is the homepage carousel-->
-<?php wp_reset_postdata();?>
+
 <div id="myCarousel" class="carousel slide">
   <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
- <?php /*?>   <li data-target="#myCarousel" data-slide-to="1"></li>
+    <?php /*?>  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+   <li data-target="#myCarousel" data-slide-to="1"></li>
     <li data-target="#myCarousel" data-slide-to="2"></li><?php */?>
   </ol>
   <!-- Carousel items -->
   <div class="carousel-inner" >
-    <?php $counter = 0;//set counter
-?>
+    <?php 
+  
+   $isFirst = true;
+
+    $i=1; 
+  ?>
     <?php 
 
 // The Query
@@ -18,26 +22,22 @@ $query = new WP_Query(array(
 	'post_status'		=> 'any',
 	'orderby'			=>'post__in'
 ));
-
+$i = 1;
 // The Loop
 if ( $query->have_posts() ) {
 	while ( $query->have_posts() ) {
-		$query->the_post();?>
-    <div class="item" >
-   
-<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-  <img src="<?php echo $image[0]; ?>" alt="" />
-
-     
-    
+		$query->the_post();
+		
+		?>
+    <div class="item  <?php if($i==1)  echo 'active';?>">
+      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+      <img src="<?php echo $image[0]; ?>"  class="img-responsive"/>
       <div class="carousel-caption">
-        <h3>
-          <?php echo the_title()?>
-        </h3>
-        <p><?php echo $feat_image ?></p>
+        <h3> <?php echo the_title()?> </h3>
+     
       </div>
     </div>
-    <?php
+    <?php  $i++;
 		
 	}
 } else {
@@ -48,7 +48,6 @@ if ( $query->have_posts() ) {
 wp_reset_postdata();?>
   </div>
   <!-- Carousel nav --> 
-  <a class="carousel-control fui-arrow-left left" href="#myCarousel" data-slide="prev"></a> <a class="carousel-control fui-arrow-right right" href="#myCarousel" data-slide="next"></a> </div>
-<div class="item "> <img src="<?php echo $feat_image ?>" >
-  <div class="carousel-caption"> </div>
-</div>
+  <a class="carousel-control fui-arrow-left left" href="#myCarousel" data-slide="prev"></a> 
+  <a class="carousel-control fui-arrow-right right" href="#myCarousel" data-slide="next"></a> </div>
+
