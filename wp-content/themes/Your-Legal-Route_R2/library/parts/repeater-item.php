df78262f7a1fb3f4f0548635bc24ac2698770161
Bill Mulholland <billmulholland@Bills-iMac-2.local>
2015-07-15 11:25:32 +0100

    homepage repeater

--- wp-content/themes/Your-Legal-Route_R2/library/parts/repeater-item.php
+++ wp-content/themes/Your-Legal-Route_R2/library/parts/repeater-item.php
@@ -3,54 +3,26 @@
 <?php $tax_term = $content[0]->term_id;?>
 <?php $tax_taxonomy = $content[0]->taxonomy;?>
 <?php $tax_name= $content[0]->name;?>
+<?php $tax_description= $content[0]->description;?>
+<?php $tax_slug= $content[0]->slug;?>
+<?php // get value from a taxonomy (taxonomy = "events", id = 76)
+$value = 'topic_'.$tax_term; ?>
 <?php /*?><?php var_dump($content);
 ?><?php */?>
+<?php 
+
+$image = get_field('tax_image',$value);
 
 
+if( !empty($image) ): ?>
 
+<img class="img-responsive img-rounded" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" />
+<?php endif; ?>
 <h6 style="margin-bottom:20px">
-<?php /*?><span style="text-transform: uppercase"><?php echo $content[0]->taxonomy;?> </span>/ <?php */?>
-<a href="/topic/<?php echo $content[0]->name;?>">
-<?php echo $content[0]->name;?></a></h6>
-
-
-<?php $i = 1;
-$query = new WP_Query(array(
-'taxonomy' => $tax_taxonomy,
-'term' => $tax_name,
-'posts_per_page' =>'4',
-
-	
-));
-
-// The Loop
-if ( $query->have_posts() ) {
-	while ( $query->have_posts() ) {
-		$query->the_post();?>
-<?php if( $i  == 1) { ?>
-<div class="First" style="border-bottom:1px solid #C7C7C7;margin-bottom:10px; clear:both;padding:10px 0px">
-  <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'bones-thumb-800' ); ?>
- <a href="<?php echo get_permalink(); ?>" ><img src="<?php echo $image[0]; ?>" alt="" class="img-responsive img-rounded"/> </a>
- <a href="<?php echo get_permalink(); ?>" > <?php echo the_title()?></a><br/>
-</div>
-<?php } else { ?>
-<div class="All" style="margin-bottom:10px; clear:both;padding:10px 0px; display:block;position:relative">
-  <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4" >
-    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'bones-thumb-800' ); ?>
-    <img src="<?php echo $image[0]; ?>" alt="" class="img-responsive img-rounded"/> 
-    </div>
-  
-  <a href="<?php echo get_permalink(); ?>" > <?php echo the_title()?></a>
- 
-</div>
-<?php } ?>
-<?php
-		$i++; 
-	}
-} else { echo "Sorry no items found";
-	// no posts found
-};?>
-<div style="clear:both"> 
-
-<hr/><sub style="text-transform: uppercase"><a href="/topic/<?php echo $content[0]->name;?>">Show all from <?php echo $content[0]->name;?></a></sub></div>
+  <?php /*?><span style="text-transform: uppercase"><?php echo $content[0]->taxonomy;?> </span>/ <?php */?>
+  <a href="/topic/<?php echo $tax_slug?>"> <?php echo $tax_name?></a></h6>
+<?php echo $tax_description?>
+<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
+<div style="clear:both"> <sub style="text-transform: uppercase; color:#919191"><a href="/topic/<?php echo $tax_slug ?>">Show all from <?php echo $content[0]->name;?></a></sub></div>
+<hr/>
 <?php wp_reset_postdata();?>
