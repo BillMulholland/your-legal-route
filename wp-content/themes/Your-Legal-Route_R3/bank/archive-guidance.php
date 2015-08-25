d7077b516faa67ccb76d15663950658ed512676a
Bill Mulholland <billmulholland@Bills-iMac-2.local>
2015-08-25 07:31:57 +0100

    forms etc

--- wp-content/themes/Your-Legal-Route_R3/bank/archive-guidance.php
+++ wp-content/themes/Your-Legal-Route_R3/bank/archive-guidance.php
@@ -1,53 +1,130 @@
 <?php get_header(); ?>
-<?php $tax_id = get_queried_object_id();?>
-<?php // get value from a taxonomy (taxonomy = "events", id = 76)
-	
-$value = 'topic_'.$tax_id ;
-$image = get_field('tax_image',$value); ?>
+<?php 
+
+$image = get_field('guidance_notes_image', 'option');
+
+?>
 
 <div class="container">
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 " >
       <div class=" img-rounded" style=" background-image:url(<?php echo $image['url']; ?>);height:460px; margin-bottom:50px">
-        <div id="cta" class="col-xs-12 col-sm-12 col-md-6 col-lg-5 col-md-offset-5 col-lg-offset-6 img-rounded" >
+        <div id="cta" class="col-xs-10 col-sm-10 col-md-6 col-lg-7 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 img-rounded" >
           <p class="lead">
-            <?php
+          <h2  class="pagetitle">
+            Guidance Notes
+            
+             <?php
 							the_archive_title( '<h2 class="pagetitle">', '</h2>' );
 							the_archive_description( '<div class="taxonomy-description">', '</div>' );
 							?>
+          </h2>
+           <span style="color:white"> Clear and simple guidance on important legal issues; covering commercial, employment and benefits, property, and regulatory compliance.</span>
           </p>
         </div>
       </div>
     </div>
   </div>
   <div class="row">
-    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-    
-    <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
-    <div class="col-xs-12 col-sm-12 col-md-6 col-lg-4 article_archive" >
-      <article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-        <header class="entry-header article-header">
-        <img class=" img-responsive img-rounded" src="<?php echo $image[0]; ?>"/>
-        
-      
-          <h3 ><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>">
-            <?php the_title(); ?>
-            </a></h3>
-        </header>
-        <section class="entry-content cf">
-          <?php /*?><?php the_post_thumbnail( 'bones-thumb-300' ); ?><?php */?>
-          <?php the_excerpt(); ?>
-          <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" class="btn btn-primary btn-xs">Visit</a> </section>
-        <hr/>
-        <?php wp_reset_postdata(); ?>
-      </article>
+    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 " >
+      <?php
+
+$args=array(
+'post_type'                => 'guidance',
+'child_of'                 => 0,
+'parent'                   => '',
+'orderby'                  => 'name',
+'order'                    => 'ASC',
+'hide_empty'               => 0,
+'hierarchical'             => 0,
+'exclude'                  => '',
+'include'                  => '',
+'number'                   => '',
+'taxonomy'                 => 'topic',
+'pad_counts'               => false
+);
+
+$categories=get_categories($args);
+
+foreach ( $categories as $category ) {
+
+if ( $category->parent > 0 ) {
+    continue;   
+}
+ $new_urlh2 = sanitize_title($category->name);
+
+echo '<hr style="clear:both"><h2> <a href="/topic/'. $new_urlh2.'">' . $category->name . '</a></h2>';
+echo category_description( $category->term_taxonomy_id ); 
+
+    $querystr = "SELECT $wpdb->posts.*
+                  FROM $wpdb->posts, $wpdb->term_relationships, $wpdb->terms
+                  WHERE term_id = (" . $category->cat_ID . ")
+                  AND term_taxonomy_id = (" . $category->term_taxonomy_id . ")
+                  AND ID = object_id
+                  AND post_type = 'guidance'
+                  AND post_status = 'publish'
+                  ORDER BY post_date DESC";
+    $posts = $wpdb->get_results($querystr, OBJECT);
+
+
+
+
+	
+
+
+
+
+
+
+
+  echo '<ul >';
+       foreach ( $posts as $post ) {
+           setup_postdata($post);  
+
+               echo '<li>';?>
+			    <a href="<?php echo get_permalink();?>">
+                <?php the_title();?>
+                </a> 
+                <?php include "library/parts/document_icons.php";  echo '</li>';
+
+                }
+   echo '</ul>';
+
+$categories2 = get_terms('topic',array('parent' => $category->term_id ));
+
+foreach ( $categories2 as $category ) {
+	
+	
+  $new_url = sanitize_title($category->name);
+ 
+
+	
+
+echo '<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 article_archive_root" ><h3> <a href="/topic/'. $new_url.'">' . $category->name . '</a></h3>';
+
+$posts = get_posts( array( 'topic' => $category->slug, 'post_type' => 'guidance' ) );  
+
+    echo '<ul>';
+        foreach($posts as $post) { 
+            setup_postdata($post);  
+		
+
+                echo '<li> '?>
+      <a href="<?php echo get_permalink();?>">
+      <?php  the_title(); 
+	  include "library/parts/document_icons.php";
+	   echo '</a></li>';
+
+                }
+    echo '</ul></div>';
+
+}
+}
+
+?>
     </div>
-    <?php endwhile; ?>
-    <?php bones_page_navi(); ?>
-    <?php else : ?>
-    <?php endif; ?>
-    </main>
   </div>
+ 
 </div>
 </div>
 <?php get_footer(); ?>
--- wp-content/themes/Your-Legal-Route_R3/page-home.php
+++ wp-content/themes/Your-Legal-Route_R3/page-home.php
@@ -35,16 +35,19 @@ if(get_field('slider_or_single_image') == "slider")
        
       </div>
     </div>
-    <div class="row">
-      <?php include "library/parts/repeater_home.php" ?>
-    </div>
-    <div class="row">
+    
+     <div class="row">
       <main id="main" class="col-xs-12 col-sm-12 col-md-12 col-lg-12 cf" > <a name="anchor"></a>
         <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
         <?php the_content();?>
         <?php endwhile; endif; ?>
       </main>
     </div>
+    
+    <div class="row">
+      <?php include "library/parts/repeater_home.php" ?>
+    </div>
+   
   </div>
 </div>
 <?php get_footer(); ?>
